9bebb4565ad85818292b6c2b34f278f81133c4fb
ahanin <alexey.hanin@gmail.com>
2012-01-18 14:54:09 +0100

    updated Apache License headers, cleaned-up maven configuration

--- getrest/src/main/java/getrest/android/service/RequestJob.java
+++ getrest/src/main/java/getrest/android/service/RequestJob.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package getrest.android.service;
 
 import getrest.android.request.Request;
